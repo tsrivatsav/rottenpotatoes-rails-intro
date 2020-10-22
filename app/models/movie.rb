e0d8b2e7f3de77d4cb8c2b7ea3333ae58000d61d
Tejas Srivatsav <tsrivatsav@berkeley.edu>
2020-10-22 04:10:23 +0000

    now

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,9 @@
 class Movie < ActiveRecord::Base
   def self.with_ratings(ratings_list)
-    if ratings_list.nil?
+    if ratings_list.empty?
       return Movie.all
-    elsif ratings_list.is_a? Array
-      return Movie.where(rating: ratings_list)
     else
-      puts 'ratings_list is something other than nil or an array'
+      return Movie.where(rating: ratings_list)
     end
   end
   def self.all_ratings
